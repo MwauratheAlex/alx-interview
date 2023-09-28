b6c388b3f864b91ca2dfa3e6cd00fbae0d7d92cf
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-28 14:38:57 +0300

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module contains the function pascal_triange(n)"""
+
+
+def pascal_triangle(n):
+    """
+    n: integer
+    Return: a list of lists of integers representing the Pascal’s triangle of n
+    returns an empty list if n <= 0
+    """
+    triangle = []
+    if n <= 0:
+        return []
+
+    for i in range(n):
+        if i <= 1:
+            triangle.append([1] * (i + 1))
+            continue
+        new_list = []
+        prev_list = triangle[i - 1]
+        # sliding window sum
+        window_sum = 0
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                window_sum += 1
+                new_list.append(1)
+            else:
+                # add current value to window sum
+                window_sum += prev_list[j]
+                new_list.append(window_sum)
+                # slide
+                window_sum -= prev_list[j - 1]
+        triangle.append(new_list)
+
+    return triangle
