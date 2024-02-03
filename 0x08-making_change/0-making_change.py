ae5e7f845ac5f04a30153719c43ceb9fc2c9f345
Mbugua <mwaurathealex@gmail.com>
2024-02-03 17:45:01 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for a in range(total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+    return dp[total] if dp[total] != total + 1 else - 1 
