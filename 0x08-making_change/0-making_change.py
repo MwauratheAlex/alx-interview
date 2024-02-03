092d7b1713eaa6c4b1970f2b9ba83aabf9e49888
Mbugua <mwaurathealex@gmail.com>
2024-02-03 18:03:55 +0300

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,7 +15,7 @@ def makeChange(coins, total):
         return 0
     dp = [total + 1] * (total + 1)
     dp[0] = 0
-    for a in range(total + 1):
+    for a in range(1, total + 1):
         for c in coins:
             if a - c >= 0:
                 dp[a] = min(dp[a], 1 + dp[a - c])
