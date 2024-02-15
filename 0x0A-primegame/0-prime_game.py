8515043bb57105aa3e51c4bd6e6d37ba40f61116
Mbugua <mwaurathealex@gmail.com>
2024-02-15 13:03:31 +0300

    optimize isPrime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,7 +12,13 @@ def isPrime(n):
     if n <= 1:
         is_prime_memo[n] = False
         return False
-    for i in range(2, n):
+    if n == 2:
+        is_prime_memo[n] = True
+        return True
+    if n % 2 == 0:
+        is_prime_memo[n] = False
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             is_prime_memo[n] = False
             return False
