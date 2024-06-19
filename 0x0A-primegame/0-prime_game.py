dee52c9276cc76f8dda0c629b361e4a06277ee26
Mbugua <mwaurathealex@gmail.com>
2024-06-19 11:49:31 +0300

    the old way

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,49 +2,48 @@
 """prime game module
 """
 
+primes_n = {}
 
-def get_primes(n, primes_memo):
+
+def get_primes(n):
     """gets the winner of a prime game
     where x is the number of rounds and nums is an array of n
     """
+    if n in primes_n:
+        return primes_n[n]
+
     primes = [True for _ in range(n + 1)]
     p = 2
-    if len(primes_memo) > 0:
-        if n < primes_memo[len(primes_memo) - 1]:
-            return
-        p = primes_memo[len(primes_memo) - 1]
 
     while (p * p) <= n:
         if primes[p]:
             for i in range(p * p, n + 1, p):
                 primes[i] = False
         p += 1
+
+    primes_n[n] = []
     for i in range(2, n + 1):
         if primes[i]:
-            if len(primes_memo) > 0:
-                if primes_memo[len(primes_memo) - 1] < i:
-                    primes_memo.append(i)
-            else:
-                primes_memo.append(i)
+            primes_n[n].append(i)
+
+    return primes_n[n]
 
 
 def isWinner(x, nums):
     """gets the winner of a prime game
     where x is the number of rounds and nums is an array of n
     """
-    primes_memo = []
-    prev_len = 0
-    winner = [0,  0]
-    streak = True
+    ben = 0
+    maria = 0
     for i in range(x):
         n = nums[i]
-        get_primes(n, primes_memo)
+        p_n = get_primes(n)
+        if len(p_n) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
 
-        if len(primes_memo) != prev_len:
-            streak = not streak
-            prev_len = len(primes_memo)
-        winner[int(streak)] += 1
-
-    if winner[0] == winner[1]:
+    if ben == maria:
         return None
-    return 'Ben' if winner[1] > winner[0] else 'Maria'
+
+    return 'Ben' if ben > maria else 'Maria'
--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -7,3 +7,11 @@ print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
 x = 3
 nums = [4, 5, 1]
 print("Winner: {}".format(isWinner(x, nums)))
+# print(isWinner(1, [1]), "BEN")  # Expected output: Ben
+# print(isWinner(4, [2, 3, 4, 5]), "MARIA")  # Expected output: Maria
+# print(isWinner(3, [7, 7, 7]), "None")  # Expected output: None
+# print(isWinner(5, [1, 2, 3, 4, 5]), "MARIA")  # Expected output: Maria
+# print(isWinner(3, [11, 13, 17]), "MARIA")  # Expected output: Maria
+# print(isWinner(4, [6, 7, 8, 9]), "BEN")  # Expected output: Ben
+# # Expected output: Maria or Ben (depends on optimal play analysis)
+# print(isWinner(2, [100, 200]))
